6241976a5889f3c6ead4a8286f8c5c82c1ccc2c0
{YutaYasumitsu} <{shiranui0317@gmail.com}>
2023-08-16 13:51:05 +0000

    main.go fix

--- app/main.go
+++ app/main.go
@@ -8,3 +8,8 @@ import (
 func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello GitOps!!")
 }
+
+func main() {
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":8080", nil)
+}
