83fe4126c49cc3b4e7b9351d8e8436762575a4a9
{YutaYasumitsu} <{shiranui0317@gmail.com}>
2023-08-19 02:27:22 +0000

    main.go fix

--- app/main.go
+++ app/main.go
@@ -6,7 +6,7 @@ import (
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello GitOps!!")
+	fmt.Fprintf(w, "Hello GitOps!!argoCD起動後にこのファイルに修正を加えて実行！！")
 }
 
 func main() {
